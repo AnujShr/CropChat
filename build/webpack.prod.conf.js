04d88a21360f9481784efaf3f2ae2559881f3f2a
Anuj <anuj.me3@gmail.com>
2018-10-09 14:35:55 +0545

    add caching

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -103,13 +103,39 @@ const webpackConfig = merge(baseWebpackConfig, {
       }
     ]),
     // service worker caching
-    new SWPrecacheWebpackPlugin({
-      cacheId: 'cropchat',
-      filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
-      minify: true,
-      stripPrefix: 'dist/'
-    })
+      new SWPrecacheWebpackPlugin({
+          cacheId: 'cropchat',
+          filename: 'service-worker.js',
+          staticFileGlobs: ['dist/**/*.{js,html,css}'],
+          minify: true,
+          stripPrefix: 'dist/',
+          runtimeCaching: [
+              {
+                  urlPattern: /^https:\/\/thecatapi\.com\/api\/images\/get\.php\?id/,
+                  handler: 'cacheFirst'
+              },
+              {
+                  urlPattern: /^https:\/\/(\d+)\.media\.tumblr\.com\//,
+                  handler: 'cacheFirst'
+              },
+              {
+                  urlPattern: /^http:\/\/(\d+)\.media\.tumblr\.com\//,
+                  handler: 'cacheFirst'
+              },
+
+              {
+                  urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+                  handler: 'cacheFirst'
+              },
+              {
+                  urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+                  handler: 'cacheFirst'
+              },
+              {
+                  urlPattern: /^https:\/\/code\.getmdl\.io\//,
+                  handler: 'cacheFirst'
+              }]
+      })
   ]
 })
 
